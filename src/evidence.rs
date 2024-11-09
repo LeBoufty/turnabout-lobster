2420cd00156b05b0383106c8ed68680104752f84
LeBoufty <boufto@gmail.com>
2024-11-09 13:24:53 +0100

    Evidence selector

--- src/evidence.rs
+++ src/evidence.rs
@@ -3,6 +3,7 @@ use toml;
 use std::fs;
 
 use crate::courtcase::Case;
+use promkit::preset::listbox::Listbox;
 
 // Struct for a piece of evidence that is stored in the court record
 #[derive(Deserialize, Debug)]
@@ -13,13 +14,51 @@ pub struct Evidence {
     weight: u8              // Location of the piece in the court record, smaller goes first.
 }
 
+impl Evidence {
+    pub fn get_fullname(&self) -> &str {
+        &self.fullname
+    }
+
+    pub fn get_alias(&self) -> &str {
+        &self.alias
+    }
+
+    pub fn get_description(&self) -> &str {
+        &self.description
+    }
+
+    pub fn get_weight(&self) -> u8 {
+        self.weight
+    }
+    
+}
+
 // Wrapper struct for deserializing the TOML file
 #[derive(Deserialize, Debug)]
-struct EvidenceList {
+pub struct EvidenceList {
     evidence: Vec<Evidence>,
 }
 
-pub fn load_evidence(case: Case) -> Vec<Evidence> {
+impl EvidenceList {
+    pub fn iter(&self) -> std::slice::Iter<Evidence> {
+        self.evidence.iter()
+    }
+    pub fn get_evidence_from_alias(&self, alias: &str) -> Option<&Evidence> {
+        self.evidence.iter().find(|e| e.get_alias() == alias)
+    }
+    pub fn get_evidence_from_fullname(&self, fullname: &str) -> Option<&Evidence> {
+        self.evidence.iter().find(|e| e.get_fullname() == fullname)
+    }
+    pub fn sort_evidence(&mut self) {
+        self.evidence.sort_by(|a, b| a.get_weight().cmp(&b.get_weight()));
+    }
+    pub fn get_evidence_list(&self) -> Vec<String> {
+        self.evidence.iter().map(|e| e.get_fullname().to_string()).collect()
+    }
+}
+
+// Load the evidence from the TOML file
+pub fn load_evidence(case: &Case) -> EvidenceList {
     let filename = format!("./cases/{}/evidence.toml", case.get_foldername());
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
@@ -27,5 +66,17 @@ pub fn load_evidence(case: Case) -> Vec<Evidence> {
     let evidence_list: EvidenceList = toml::from_str(&contents)
         .expect("Failed to parse TOML");
 
-    evidence_list.evidence
+    evidence_list
+}
+
+pub fn select_evidence(evidence_list: &EvidenceList) -> Result<&Evidence, Box<dyn std::error::Error>> {
+    let mut p = Listbox::new(evidence_list.get_evidence_list())
+        .title("Court record")
+        .listbox_lines(10)
+        .prompt()?;
+    if let Some(e) = evidence_list.get_evidence_from_fullname(&p.run()?) {
+        Ok(e)
+    } else {
+        Err("Failed to find evidence".into())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod evidence;
 mod courtcase;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cases = courtcase::load_cases();
-    println!("{:?}", cases);
+    let evidence = evidence::load_evidence(&cases[0]);
+    let selected_evidence = evidence::select_evidence(&evidence)?;
+    println!("Selected evidence: {}", selected_evidence.get_fullname());
+    Ok(())
 }
